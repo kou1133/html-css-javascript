67950c55666e50d4a8f1a2f4f9a4149d463110da
Kosei Tajima <109095504+kou1133@users.noreply.github.com>
2022-11-11 21:04:31 +0900

    Add files via upload

---
+++ Shooting/Shooting.java
@@ -0,0 +1,11 @@
+
+
+public class Shooting{
+    public static ShootingFrame ShootingFrame;
+
+    public static void main (String[] args){
+        ShootingFrame = new ShootingFrame();
+    }
+        
+}
+
